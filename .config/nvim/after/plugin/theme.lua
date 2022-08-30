347dff0a94bc52ad236f7b9b872f310938156dc6
Marc <marcjohnfong@gmail.com>
2022-08-30 10:09:21 +0800

    Update theme.lua

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -1,12 +1,3 @@
 vim.opt.termguicolors = true
-vim.cmd("colorscheme dawnfox")
-
-require"nightfox".setup({
-  options = {
-    styles = {
-      comments = "italic",
-      keywords = "bold",
-      types = "italic,bold",
-    }
-  }
-})
+vim.opt.background = "dark"
+vim.cmd("colorscheme gruvbox")
