78d69c579afc0e9a4b464dfbba40d20ece89b1e6
marcfyk <marcjohnfong@gmail.com>
2022-10-26 01:28:09 +0800

    Update theme

--- .config/nvim/after/plugin/theme.lua
+++ .config/nvim/after/plugin/theme.lua
@@ -1,2 +1,13 @@
 vim.opt.termguicolors = true
+
+require"nightfox".setup{
+  options = {
+    styles = {
+      comments = "italic",
+      keywords = "bold",
+      types = "italic,bold",
+    }
+  }
+}
+
 vim.cmd("colorscheme dawnfox")
