12cf8233606fdb4950e1a33046b29bd92a08717d
marcfyk <marcjohnfong@gmail.com>
2023-02-06 08:53:54 +0800

    Add custom diagnostic for bufferline

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,2 +1,13 @@
 require "bufferline".setup {
+  options = {
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+            or (e == "warning" and " " or "")
+        s = s .. n .. sym
+      end
+      return s
+    end,
+  },
 }
