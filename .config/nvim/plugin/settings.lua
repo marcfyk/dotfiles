ec34f3f91a03361848e22450334a67ab0b159e06
Marc <marcjohnfong@gmail.com>
2023-02-13 10:31:59 +0800

    Update listchars for vim/neovim

--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -29,7 +29,8 @@ vim.opt.splitbelow = true
 
 vim.opt.list = true
 vim.opt.listchars:append "eol:↲"
-vim.opt.listchars:append "tab:<->"
+vim.opt.listchars:append "tab:|->"
 vim.opt.listchars:append "space:·"
+vim.opt.listchars:append "multispace:|>"
 
 vim.opt.termguicolors = true
