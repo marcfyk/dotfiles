aa89e4f571aa3ec06d2351a8b1949175bdbcad5a
Marc <marcjohnfong@gmail.com>
2023-02-13 10:47:55 +0800

    Remove multispace for vim/nvim

--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -31,6 +31,5 @@ vim.opt.list = true
 vim.opt.listchars:append "eol:↲"
 vim.opt.listchars:append "tab:|->"
 vim.opt.listchars:append "space:·"
-vim.opt.listchars:append "multispace:|>"
 
 vim.opt.termguicolors = true
