466af1cf536a431023eb7986a1b7d227b044f7e3
marcfyk <marcjohnfong@gmail.com>
2023-02-19 20:59:35 +0800

    Update dotfiles zsh

--- .config/zsh/dotfiles.zsh
+++ .config/zsh/dotfiles.zsh
@@ -1,1 +1,4 @@
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# Prevents showing untracked files in the $HOME directory when running `dotfiles status`
+dotfiles config --local status.showUntrackedFiles no
