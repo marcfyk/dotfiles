cdf5b11f17f918d69be2cf57cb949b40e1919780
marcfyk <marcjohnfong@gmail.com>
2023-03-05 13:01:42 +0800

    Update telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,16 +1,11 @@
 local telescope = require "telescope"
-telescope.setup {
-  pickers = {
-    find_files = {
-      hidden = true
-    }
-  }
-}
+telescope.setup {}
 telescope.load_extension "fzf"
 telescope.load_extension "file_browser"
 
 require "which-key".register({
   ["<leader>ff"] = { "<cmd> Telescope find_files<CR>", "Find File" },
+  ["<leader>fh"] = { "<cmd> Telescope find_files hidden=true<CR>", "Find File" },
   ["<leader>fb"] = { "<cmd> Telescope file_browser<CR>", "File Browser" },
   ["<leader>fg"] = { "<cmd> Telescope live_grep<CR>", "Live Grep" },
 })
