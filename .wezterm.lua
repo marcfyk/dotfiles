2e92e7ad4ca0a66822ac239e7e4eb29c03204e82
Marc <marcjohnfong@gmail.com>
2023-07-28 13:34:59 +0800

    Add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require "wezterm"
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "tokyonight_moon"
+config.font = wezterm.font "JetBrainsMono Nerd Font Mono"
+
+return config
