9c37807298fb32218c00757ab241f90613c70c6d
marcfyk <marcjohnfong@gmail.com>
2023-08-05 02:18:15 +0800

    Update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "tokyonight_day"
+config.color_scheme = "Everforest Dark (Gogh)"
 config.font = wezterm.font "JetBrainsMono Nerd Font Mono"
 
 return config
