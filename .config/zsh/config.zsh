3eba14c0df6914102e148f409f5c6fea70c2a8a7
Marc Fong <marcjohnfong@gmail.com>
2023-08-28 21:39:28 +0800

    Add neofetch and consolidate zshrc

---
+++ .config/zsh/config.zsh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+##### Sets up dotfiles #####
+
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+# Prevents showing untracked files in the $HOME directory when running `dotfiles status`
+dotfiles config --local status.showUntrackedFiles no
+
+
+##### Antigen plugin manager for zsh #####
+
+if [ ! -f $HOME/antigen.zsh ]; then
+    curl -L git.io/antigen > $HOME/antigen.zsh
+fi
+
+source $HOME/antigen.zsh
+
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
+
+antigen theme spaceship-prompt/spaceship-prompt
+
+antigen apply
+
+
+##### Neofetch #####
+
+[[ -f $(which neofetch) ]] && neofetch
+
+##########
--- .config/zsh/dotfiles.zsh
+++
@@ -1,4 +0,0 @@
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-
-# Prevents showing untracked files in the $HOME directory when running `dotfiles status`
-dotfiles config --local status.showUntrackedFiles no
