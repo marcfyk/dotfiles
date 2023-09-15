46ff3b73eecd652fa5cd8fb8fdf1cb70face2f2f
Marc Fong <marcjohnfong@gmail.com>
2023-09-16 00:00:32 +0800

    Add theme for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "Github Dark"
+config.color_scheme = "Kanagawa (Gogh)"
 config.font = wezterm.font "BlexMono Nerd Font"
 
 return config
