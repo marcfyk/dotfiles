061ce81c65c271b1ca981311cfd43d6bee510353
Marc Fong <marcjohnfong@gmail.com>
2023-12-10 16:12:10 +0800

    Add wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "flexoki-dark"
 config.font = wezterm.font "BlexMono Nerd Font"
 
 return config
