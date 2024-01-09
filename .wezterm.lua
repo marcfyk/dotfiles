fd2c8770ec66f58ec8fd834e2fd0560264da249d
Marc Fong <marcjohnfong@gmail.com>
2024-01-09 13:34:22 +0800

    Update Catppuccin wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font "JetBrainsMono Nerd Font"
 
 return config
