befd8c0f592c55f739da550126927ff88e0a18c6
Marc Fong <marcjohnfong@gmail.com>
2024-02-05 20:53:23 +0800

    Update listchars for nvim

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -31,5 +31,6 @@ vim.opt.splitbelow = true
 
 vim.opt.list = true
 vim.opt.listchars:append "eol:↲"
-vim.opt.listchars:append "tab:|-"
+vim.opt.listchars:append "tab:▸ "
 vim.opt.listchars:append "space:·"
+vim.opt.listchars:append "trail:×"
