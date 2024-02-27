4e75351cb952d138a1ed3a5b7d165ff7f872a8cb
Marc Fong <marcjohnfong@gmail.com>
2024-02-28 01:58:33 +0800

    Add diagnostics keybind to nvim

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -7,6 +7,7 @@ require "which-key".register({
   ["<leader>ff"] = { "<cmd> Telescope find_files<CR>", "Find File" },
   ["<leader>fd"] = { "<cmd> Telescope find_files hidden=true<CR>", "Find File" },
   ["<leader>fg"] = { "<cmd> Telescope live_grep<CR>", "Live Grep" },
+  ["<leader>ft"] = { "<cmd> Telescope diagnostics<CR>", "Diagnostics"},
   ["<leader>fh"] = { "<cmd> Telescope help_tags<CR>", "Help tags" },
   ["<leader>fb"] = { "<cmd> Telescope file_browser<CR>", "File Browser" },
 })
