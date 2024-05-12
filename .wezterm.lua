720664cd4c99ad0ea10242818dfcc716a7f73baa
Marc Fong <marcjohnfong@gmail.com>
2024-05-12 19:12:18 +0800

    Fix wezterm font

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,6 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "rose-pine"
-config.font = wezterm.font "FantasqueSansMono Nerd Font"
+config.font = wezterm.font "FantasqueSansM Nerd Font Mono"
 
 return config
