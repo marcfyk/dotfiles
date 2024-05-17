37cf097e91d6b918dd1e8cbc8d18eebf3ebcc144
Marc Fong <marcjohnfong@gmail.com>
2024-05-17 12:52:20 +0800

    Update telescope themes

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -152,6 +152,40 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local telescope = require "telescope"
+      telescope.setup {
+        pickers = {
+          find_files = {
+            theme = "ivy",
+          },
+          live_grep = {
+            theme = "ivy",
+          },
+          diagnostics = {
+            theme = "ivy",
+          },
+          help_tags = {
+            theme = "ivy",
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+          }
+        }
+      }
+      telescope.load_extension "fzf"
+      telescope.load_extension "file_browser"
+      require "which-key".register({
+        ["<leader>ff"] = { "<cmd>Telescope find_files<CR>", "Find File" },
+        ["<leader>fd"] = { "<cmd>Telescope find_files hidden=true<CR>", "Find File" },
+        ["<leader>fg"] = { "<cmd>Telescope live_grep<CR>", "Live Grep" },
+        ["<leader>ft"] = { "<cmd>Telescope diagnostics<CR>", "Diagnostics" },
+        ["<leader>fh"] = { "<cmd>Telescope help_tags<CR>", "Help tags" },
+        ["<leader>fb"] = { "<cmd>Telescope file_browser<CR>", "File Browser" },
+      })
+    end
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
