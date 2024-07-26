7545605ccc98433239d93a69b43b722562d5f8b4
Marc Fong <marcjohnfong@gmail.com>
2024-07-26 09:51:38 +0800

    Add symbol outline plugin to nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -213,6 +213,20 @@ return {
     },
   },
 
+  -- symbol outline
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
+
   -- Git
   "tpope/vim-fugitive",
   {
