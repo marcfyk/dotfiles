ad8736b7b856ede979a2bf33d945572dce0876c8
Marc Fong <marcjohnfong@gmail.com>
2024-09-16 09:42:10 +0800

    Add zenbones to neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -95,7 +95,17 @@ return {
           }
         end,
       })
-      vim.cmd("colorscheme kanagawa")
+    end
+  },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    config = function ()
+      vim.opt.background = "dark"
+      vim.cmd("colorscheme zenbones")
     end
   },
 
