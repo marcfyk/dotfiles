9972ded76ce6666358da3a772974c2bd8acd118f
Marc Fong <marcjohnfong@gmail.com>
2024-09-16 09:42:32 +0800

    Update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "Kanagawa Dragon (Gogh)"
+config.color_scheme = "zenbones"
 config.font = wezterm.font "ZedMono Nerd Font"
 config.font_size = 16
 config.window_background_opacity = 1.0
