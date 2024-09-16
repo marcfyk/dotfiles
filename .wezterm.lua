9ff72a942de697e4c644107cfa6bad30232e8f5c
Marc Fong <marcjohnfong@gmail.com>
2024-09-16 09:44:00 +0800

    Update wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = "zenbones"
+config.color_scheme = "zenbones_dark"
 config.font = wezterm.font "ZedMono Nerd Font"
 config.font_size = 16
 config.window_background_opacity = 1.0
