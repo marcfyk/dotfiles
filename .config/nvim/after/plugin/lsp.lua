e10982dbc3adc09ce4d09b9f15b72f838a5c7720
Marc Fong <marcjohnfong@gmail.com>
2024-11-15 22:02:52 +0800

    Remove unused nvim-cmp setup for git

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -33,15 +33,6 @@ cmp.setup({
   })
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype("gitcommit", {
-  sources = cmp.config.sources({
-    { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = "buffer" },
-  })
-})
-
 -- setup for vim-dadbod.
 cmp.setup.filetype({ "sql" }, {
   sources = {
