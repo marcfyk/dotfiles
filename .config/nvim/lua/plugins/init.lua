32b12db3661013ef2d79eba02c1398f4654e836c
Marc Fong <marcjohnfong@gmail.com>
2025-04-08 01:19:50 +0800

    Set borderless highlights to tokyonight on nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -146,6 +146,39 @@ return {
   {
     "folke/tokyonight.nvim",
     config = function()
+      require("tokyonight").setup({
+        on_highlights = function(hl, c)
+          local prompt = "#2d3149"
+          hl.TelescopeNormal = {
+            bg = c.bg_dark,
+            fg = c.fg_dark,
+          }
+          hl.TelescopeBorder = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopePromptNormal = {
+            bg = prompt,
+          }
+          hl.TelescopePromptBorder = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePromptTitle = {
+            bg = prompt,
+            fg = prompt,
+          }
+          hl.TelescopePreviewTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+          hl.TelescopeResultsTitle = {
+            bg = c.bg_dark,
+            fg = c.bg_dark,
+          }
+        end,
+      })
+      vim.cmd("colorscheme tokyonight")
     end
   },
   {
@@ -155,7 +188,6 @@ return {
         italic_comments = true,
         borderless_pickers = true,
       })
-      vim.cmd("colorscheme cyberdream")
     end
   },
 
