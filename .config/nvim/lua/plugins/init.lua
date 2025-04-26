7052835e34b4e8cf82d06762d282ac4a31c8ef16
Marc Fong <marcjohnfong@gmail.com>
2025-04-26 16:30:27 +0800

    Update which-key plugin for nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -197,10 +197,18 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
+    opts = {
+      preset = "helix",
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      }
+    },
   },
 
   -- Treesitter
