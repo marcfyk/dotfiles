fb3b35c571b1973d8e204545ca099cb41976da4b
Marc Fong <marcjohnfong@gmail.com>
2025-06-03 16:35:47 +0800

    Update treesitter plugin setup in neovim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -20,18 +20,9 @@ return {
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        auto_install = true,
-        ensure_installed = {},
-        highlight = { enable = true },
-        ignore_install = {},
-        indent = { enable = true },
-        sync_install = false,
-      })
-    end
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
