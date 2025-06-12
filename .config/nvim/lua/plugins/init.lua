8836b4e7562c31b758c021ee57baa4bdd4fdbb60
Marc Fong <marcjohnfong@gmail.com>
2025-06-12 17:13:28 +0800

    Update treesitter setup for neovim
    - Add several ensured parsers in treesitter setup.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -23,6 +23,11 @@ return {
     branch = "master",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", }
+      })
+    end
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
