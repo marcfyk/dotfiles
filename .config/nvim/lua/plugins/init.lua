146f498e0ce7a4fb3c134d332473c8017162bdc3
Marc Fong <marcjohnfong@gmail.com>
2025-06-12 17:14:41 +0800

    Add lazydev plugin to neovim
    - Remove deprecated neodev plugin.
    - Add lazydev plugin as a replacement to neodev.

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -248,7 +248,17 @@ return {
   },
 
   -- neovim API
-  { "folke/neodev.nvim", opts = {} },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
 
   -- bookmarks
   {
