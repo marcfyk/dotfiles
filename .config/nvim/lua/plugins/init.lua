2515c4b032a4763dd0ec5b166718cd14d610f84a
Marc Fong <marcjohnfong@gmail.com>
2025-09-09 15:49:40 +0800

    Add format keybind to LSP

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -138,6 +138,9 @@ return {
           --  the definition of its *type*, not where it was *defined*.
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
+          -- Format buffer
+          map('gF', function() vim.lsp.buf.format({ async = true }) end, '[F]ormat')
+
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
